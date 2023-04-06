a12f8496f347877cfe1df473a18685979ff250a8
David <93221294+DavidLangat@users.noreply.github.com>
2023-04-06 17:56:15 +0300

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,7 @@
 Distributes an archive to your web servers using Fabric
 """
 
-from fabric.api import env, put, run, sudo
+from fabric.api import env, put, run
 from os.path import exists
 import os
 
@@ -22,20 +22,20 @@ def do_deploy(archive_path):
     # extract archive to /data/web_static/releases/<archive filename without extension>
     filename = os.path.basename(archive_path)
     foldername = filename.split('.')[0]
-    run('sudo mkdir -p /data/web_static/releases/{}/'.format(foldername))
-    run('sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(filename, foldername))
+    run('mkdir -p /data/web_static/releases/{}/'.format(foldername))
+    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(filename, foldername))
 
     # delete archive from web server
-    run('sudo rm /tmp/{}'.format(filename))
+    run('rm /tmp/{}'.format(filename))
 
     # move contents of foldername to parent directory
-    run('sudo mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(foldername, foldername))
-    run('sudo rm -rf /data/web_static/releases/{}/web_static'.format(foldername))
+    run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(foldername, foldername))
+    run('rm -rf /data/web_static/releases/{}/web_static'.format(foldername))
 
     # delete symbolic link /data/web_static/current from web server
-    run('sudo rm -rf /data/web_static/current')
+    run('rm -rf /data/web_static/current')
 
     # create new symbolic link
-    run('sudo ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(foldername))
+    run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(foldername))
 
     return True
