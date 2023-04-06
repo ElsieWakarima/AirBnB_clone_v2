b595b78ad42e3daebe73f6180cb18232a9ca1542
David <93221294+DavidLangat@users.noreply.github.com>
2023-04-06 17:55:05 +0300

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,7 @@
 Distributes an archive to your web servers using Fabric
 """
 
-from fabric.api import env, put, run
+from fabric.api import env, put, run, sudo
 from os.path import exists
 import os
 
@@ -11,23 +11,31 @@ env.hosts = ['54.174.248.63', '18.206.206.78']
 env.user = 'ubuntu'  
 
 def do_deploy(archive_path):
-    """Deploys archive to web servers"""
+   """Deploys archive to web servers"""
 
     if not exists(archive_path):
         return False
-        
-    try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
-    except:
-        return False
+
+    # upload archive to tmp directory of web server
+    put(archive_path, '/tmp/')
+
+    # extract archive to /data/web_static/releases/<archive filename without extension>
+    filename = os.path.basename(archive_path)
+    foldername = filename.split('.')[0]
+    run('sudo mkdir -p /data/web_static/releases/{}/'.format(foldername))
+    run('sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(filename, foldername))
+
+    # delete archive from web server
+    run('sudo rm /tmp/{}'.format(filename))
+
+    # move contents of foldername to parent directory
+    run('sudo mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(foldername, foldername))
+    run('sudo rm -rf /data/web_static/releases/{}/web_static'.format(foldername))
+
+    # delete symbolic link /data/web_static/current from web server
+    run('sudo rm -rf /data/web_static/current')
+
+    # create new symbolic link
+    run('sudo ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(foldername))
+
+    return True
