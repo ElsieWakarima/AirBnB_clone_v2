9af3d93dc625219424a0806ef18721e22b9ad55b
David <93221294+DavidLangat@users.noreply.github.com>
2023-04-06 16:48:25 +0300

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,18 +17,18 @@ def do_deploy(archive_path):
     no_ext = file_n.split(".")[0]
     path = "/data/web_static/releases/"
     put(archive_path, '/tmp/')
-    if run('mkdir -p {}{}/'.format(path, no_ext)).pass:
+    if run('mkdir -p {}{}/'.format(path, no_ext)).passed:
         return True
-    if run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext)).pass:
+    if run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext)).passed:
         return True
-    if run('rm /tmp/{}'.format(file_n)).pass:
+    if run('rm /tmp/{}'.format(file_n)).passed:
         return True
-    if run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext)).pass:
+    if run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext)).passed:
         return True
-    if run('rm -rf {}{}/web_static'.format(path, no_ext)).pass:
+    if run('rm -rf {}{}/web_static'.format(path, no_ext)).passed:
         return True
-    if run('rm -rf /data/web_static/current').pass:
+    if run('rm -rf /data/web_static/current').passed:
         return True
-    if run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext)).pass:
+    if run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext)).passed:
         return True
     return False
