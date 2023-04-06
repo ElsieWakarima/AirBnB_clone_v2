4ca29f7f59c8b45c67b8fa21c7199f2b38801914
David <93221294+DavidLangat@users.noreply.github.com>
2023-04-06 17:43:44 +0300

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,58 +1,41 @@
-#!/usr/bin/python3
-"""pack and deploy content to server
+#!/usr/bin/env python3
 """
-from fabric.api import local, env, run, put
-from datetime import datetime
+Distributes an archive to your web servers using Fabric
+"""
+
+from fabric.api import env, put, run, sudo
+from os.path import exists
 import os
+
 env.hosts = ['54.174.248.63', '18.206.206.78']
-env.user = 'ubuntu'
-
-
-def do_pack():
-    """pack all content within web_static
-    into a .tgz archive
-    The archive will be put in versions/
-    """
-    if not os.path.exists("versions"):
-        local("mkdir versions")
-    now = datetime.now()
-    name = "versions/web_static_{}.tgz".format(
-        now.strftime("%Y%m%d%H%M%S")
-    )
-    cmd = "tar -cvzf {} {}".format(name, "web_static")
-    result = local(cmd)
-    if not result.failed:
-        return name
-
+env.user = 'ubuntu'  
 
 def do_deploy(archive_path):
-    """deploy package to remote server
-    Arguments:
-        archive_path: path to archive to deploy
-    """
-    if not archive_path or not os.path.exists(archive_path):
+    """Deploys archive to web servers"""
+
+    if not exists(archive_path):
         return False
-    put(archive_path, '/tmp')
-    ar_name = archive_path[archive_path.find("/") + 1: -4]
-    try:
-        run('mkdir -p /data/web_static/releases/{}/'.format(ar_name))
-        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'.format(
-                ar_name, ar_name
-        ))
-        run('rm /tmp/{}.tgz'.format(ar_name))
-        run('mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/'.format(
-                ar_name, ar_name
-        ))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(
-            ar_name
-        ))
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ \
-            /data/web_static/current'.format(
-            ar_name
-        ))
-        print("New version deployed!")
-        return True
-    except:
-        return False
+
+    # upload archive to tmp directory of web server
+    put(archive_path, '/tmp/')
+
+    # extract archive to /data/web_static/releases/<archive filename without extension>
+    filename = os.path.basename(archive_path)
+    foldername = filename.split('.')[0]
+    run('sudo mkdir -p /data/web_static/releases/{}/'.format(foldername))
+    run('sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(filename, foldername))
+
+    # delete archive from web server
+    run('sudo rm /tmp/{}'.format(filename))
+
+    # move contents of foldername to parent directory
+    run('sudo mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(foldername, foldername))
+    run('sudo rm -rf /data/web_static/releases/{}/web_static'.format(foldername))
+
+    # delete symbolic link /data/web_static/current from web server
+    run('sudo rm -rf /data/web_static/current')
+
+    # create new symbolic link
+    run('sudo ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(foldername))
+
+    return True
